144b1d38f9789ede3a8e52496d975f5a36492fbd
Iman <imansingh96@gmail.com>
2017-07-28 14:05:20 -0400

    Add files via upload

--- learn1.py
+++ learn1.py
@@ -96,54 +96,55 @@ def load_corpus(file, block_size):
 
 music, rate = wav_to_np('./notstatic/Damiano_Baldoni_-_Ive_not_fear.wav')
 
-#wav.write('new.wav', rate, music)
-chunks_X = []
-chunks_Y = []
-X, Y_t = load_corpus('./notstatic/Damiano_Baldoni_-_Ive_not_fear.wav', block_size=2048)
-print(type(Y_t), type(Y_t[0]), type(Y_t[0][0]), type(Y_t[0][0][0]))
+if __name__ == '__main__':
+	#wav.write('new.wav', rate, music)
+	chunks_X = []
+	chunks_Y = []
+	X, Y_t = load_corpus('./notstatic/Damiano_Baldoni_-_Ive_not_fear.wav', block_size=2048)
+	print(type(Y_t), type(Y_t[0]), type(Y_t[0][0]), type(Y_t[0][0][0]))
 
-#Not sure if necessary yet
-'''cur_seq = 0
-total_seq = len(X)
-print total_seq
-print max_seq_len
-while cur_seq + max_seq_len < total_seq:
-	chunks_X.append(X[cur_seq:cur_seq+max_seq_len])
-	chunks_Y.append(Y[cur_seq:cur_seq+max_seq_len])
-	cur_seq +x = seed #is empty...
-= max_seq_len'''
+	#Not sure if necessary yet
+	'''cur_seq = 0
+	total_seq = len(X)
+	print total_seq
+	print max_seq_len
+	while cur_seq + max_seq_len < total_seq:
+		chunks_X.append(X[cur_seq:cur_seq+max_seq_len])
+		chunks_Y.append(Y[cur_seq:cur_seq+max_seq_len])
+		cur_seq +x = seed #is empty...
+	= max_seq_len'''
 
-#seed = X[:][738:748]
-#tries to do brain stuff
-print('Building brain...')
-model = Sequential()
-model.add(LSTM(2048, input_shape=(4096, 2), return_sequences=True))
-model.add(Dropout(0.2))
-#model.add(LSTM(2048, input_shape=(4096, 2), return_sequences=False))
-#model.add(Dropout(0.2))
-model.add(Dense(2))
-model.add(Activation('linear'))
+	#seed = X[:][738:748]
+	#tries to do brain stuff
+	print('Building brain...')
+	model = Sequential()
+	model.add(LSTM(2048, input_shape=(4096, 2), return_sequences=True))
+	model.add(Dropout(0.2))
+	#model.add(LSTM(2048, input_shape=(4096, 2), return_sequences=False))
+	#model.add(Dropout(0.2))
+	model.add(Dense(2))
+	model.add(Activation('linear'))
 
-print('Braining...')
-optimizer = RMSprop(lr=0.01)
-model.compile(loss='mse', optimizer='rmsprop')
-model.fit(X, Y_t, batch_size=441000, epochs=1, verbose=2)
+	print('Braining...')
+	optimizer = RMSprop(lr=0.01)
+	model.compile(loss='mse', optimizer='rmsprop')
+	model.fit(X, Y_t, batch_size=441000, epochs=1, verbose=2)
 
-print('Doing brain stuff...')
-prediction = []
-#x = seed #is empty...
-print(X)
-print('#######\n')
+	print('Doing brain stuff...')
+	prediction = []
+	#x = seed #is empty...
+	print(X)
+	print('#######\n')
 
-for ind in xrange(40):
-	preds = model.predict(X)
-	print(preds)
-	print(len(preds))
+	for ind in xrange(40):
+		preds = model.predict(X)
+		print(preds)
+		print(len(preds))
 
-	#copy-pasta'd from somewhere. Does not seem to help in my case
-	x = np.squeeze(x)
-	x = np.concatenate((x, preds))
-	x = x[1:]
-	x = np.expand_dims(x, axis=0)
-	preds = np.squeeze(preds)
-	prediction.append(preds)
+		#copy-pasta'd from somewhere. Does not seem to help in my case
+		x = np.squeeze(x)
+		x = np.concatenate((x, preds))
+		x = x[1:]
+		x = np.expand_dims(x, axis=0)
+		preds = np.squeeze(preds)
+		prediction.append(preds)
